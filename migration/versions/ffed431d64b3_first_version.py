5439cb445966b16b794fc0b44f2f4909aa99c0ea
Alveeeep <alvir.misalimov@gmail.com>
2025-07-09 00:00:20 +0300

    added alembic migration

---
+++ migration/versions/ffed431d64b3_first_version.py
@@ -0,0 +1,28 @@
+"""first_version
+
+Revision ID: ffed431d64b3
+Revises: 
+Create Date: 2025-07-08 23:59:19.056642
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'ffed431d64b3'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
