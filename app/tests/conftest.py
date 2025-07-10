74684e1eea8af419f1a738078c156fe5ff3e36c4
Alveeeep <alvir.misalimov@gmail.com>
2025-07-10 22:52:57 +0300

    Updated docker

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,19 +1,8 @@
-import asyncio
-from pathlib import Path
-
-import pytest
 import pytest_asyncio
-from alembic import command
-from alembic.config import Config
 from httpx import ASGITransport, AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
-from app.config import settings
-from app.database.db import Base, engine
-from app.dependencies.dao_dep import (
-    get_session_with_commit,
-    get_session_without_commit,
-)
+from app.database.db import engine
 from app.main import app
 
 
@@ -21,39 +10,6 @@ from app.main import app
 async_session_maker = async_sessionmaker(
     engine, autoflush=False, expire_on_commit=False
 )
-
-
-@pytest.fixture(scope="session")
-def event_loop():
-    loop = asyncio.new_event_loop()
-    yield loop
-    loop.close()
-
-
-async def apply_migrations():
-    alembic_cfg = Config()
-    alembic_cfg.set_main_option("script_location", "alembic")
-    alembic_cfg.set_main_option("sqlalchemy.url", str(settings.DB_URL))
-
-    async with engine.begin() as conn:
-        await conn.run_sync(lambda c: command.upgrade(alembic_cfg, "head"))
-
-
-@pytest.fixture(scope="session", autouse=True)
-async def prepare_database():
-    # Очистка старых миграций
-    migrations_dir = Path("alembic/versions")
-    if migrations_dir.exists():
-        for f in migrations_dir.glob("*.py"):
-            f.unlink()
-
-    # Применение миграций
-    await apply_migrations()
-    yield
-
-    # Очистка после тестов
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
 
 
 @pytest_asyncio.fixture
@@ -79,25 +35,7 @@ async def db_session_with_commit() -> AsyncSession:
 
 @pytest_asyncio.fixture
 async def client():
-    async def override_get_db():
-        async with async_session_maker() as session:
-            async with session.begin():
-                try:
-                    yield session
-                except Exception:
-                    await session.rollback()
-                    raise
-
-    app.dependency_overrides.update(
-        {
-            get_session_with_commit: override_get_db,
-            get_session_without_commit: override_get_db,
-        }
-    )
-
     async with AsyncClient(
         transport=ASGITransport(app=app), base_url="http://test"
     ) as client:
         yield client
-
-    app.dependency_overrides.clear()
